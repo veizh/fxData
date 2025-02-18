fd676918a428ec1b36239a9bce24ac6d7f499ad5
Veizh <maximedurville95300@gmail.com>
2025-02-18 21:34:40 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,115 @@
+
+let button = document.querySelector('.button-action')
+let input = document.querySelector('.arraytext')
+let nameNotation =document.querySelector('.playerNameNotation')
+let totalMatch = 0
+let totalWin = 0
+let historicArray = []
+let limit = 10
+document.querySelector('.refresh').addEventListener('click',()=>{
+    location.reload();
+})
+function splitFunction(text,limit){
+    let tmp = text.split(/\r?\n|\r|\n/g)
+    console.log("split by line " , tmp);
+    
+    tmp.forEach((element,i) => {
+        if(i>9) return
+        const t = element.split('\t')
+        let x = {}
+        t.forEach((e,i)=>{
+            if(i===6){
+                let tmp =e.substring(0,nameNotation.value.length+3) 
+                if(tmp.includes(nameNotation.value)){
+                    totalWin+=1
+                    x.win=true
+
+                }
+                else{
+                    x.win=false
+
+                    totalMatch +=1
+                }
+                
+            }
+
+            if(i===7){
+                
+                    x.set=nombreDeSets(e)
+
+            setTimeout(() => {
+                historicArray.push(x)
+            }, 150);
+
+            }
+        })
+    });
+    console.log('====================================');
+    console.log(totalWin, "nombre de win sur ", totalMatch , " matchs");
+    console.log('====================================');
+    return tmp
+}
+button.addEventListener('click',(e)=>{
+    e.preventDefault()
+    console.log('====================================');
+    console.log(input.value);
+    console.log('====================================');
+    splitFunction(input.value);
+    setTimeout(() => {
+        displayResult(historicArray)
+    }, 500);
+})
+function nombreDeSets(score) {
+    // Remplace les virgules par des espaces et supprime les espaces inutiles
+    score = score.replace(/[,]+/g, " ").trim();
+
+    // Supprime les annotations entre parenthèses (ex: "(ch)")
+    score = score.replace(/\([^)]*\)/g, "").trim();
+
+    // Séparer les sets en tenant compte des annotations comme RET, WO, AB
+    let sets = score.split(/\s+/).filter(set => !/^[A-Z]+$/.test(set));
+
+    return sets.length; // Retourne le nombre de sets valides
+}
+function displayResult(array){
+    let section = document.querySelector('.displayResults')
+    let percent1 = document.querySelector('.percent')
+    let percent2 = document.querySelector('.percent2')
+
+    array.forEach((e,i)=>{
+        let t = document.createElement('div')
+        t.classList.add('pastille')
+        console.log('====================================');
+        console.log(e);
+        console.log('====================================');
+        t.innerHTML=e.set
+        if(e.win){
+            t.classList.add('green')
+        }
+        if(!e.win){
+            t.classList.add('red')
+        }
+        section.appendChild(t)
+    })
+    percent1.innerHTML = calculPercentage(historicArray,5) + " => taux de victoire sur les 5 derniers matchs"
+    percent2.innerHTML = calculPercentage(historicArray,10) +" => taux de victoire sur les 10 derniers matchs"
+
+}
+function calculPercentage(array,limit){
+    let l =0
+    let w =0
+    array.forEach((e,i)=>{
+        if(i>limit-1) return
+        if(e.win===false){
+            l+=1
+        }
+        if(e.win===true){
+            w+=1
+        }
+
+    })
+    console.log(l);
+    console.log(w);
+    
+    return (w/(w+l))
+}
